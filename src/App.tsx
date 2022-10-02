e3b1a7e5713c8260929db836fdceaff436dae994
rwa <rwakulszowa1@gmail.com>
2022-10-02 19:29:56 +0200

    feat: sql.js integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,67 @@
+import { useState, useEffect } from "react";
+
+import initSqlJs from "sql.js";
+import wasmURL from "url:sql.js/dist/sql-wasm.wasm";
+
+type DbResult = string | SQL.Database;
+
 export function App() {
-  return <h1 className="lead text-blue-500">Hello world!</h1>;
+  const [dbResult, setDbResult] = useState<DbResult>("Loading");
+
+  useEffect(() => {
+    async function connectDb() {
+      try {
+        const SQL = await initSqlJs({ locateFile: () => wasmURL });
+        setDbResult(new SQL.Database());
+      } catch (err) {
+        setDbResult(err);
+      }
+    }
+    connectDb();
+  }, []);
+
+  if (typeof dbResult === "string") {
+    const error = dbResult;
+    return <pre>{error.toString()}</pre>;
+  } else {
+    const db = dbResult;
+    db.run(`
+        CREATE TABLE pets (name string, size int);
+        INSERT INTO pets VALUES ("Azor", 15), ("Bonifacy", 5);
+      `);
+
+    const result = db.exec("SELECT * FROM pets");
+    return (
+      <pre>
+        <ResultsTable table={result[0]} />
+      </pre>
+    );
+  }
+}
+
+function ResultsTable({ table }) {
+  const { columns, values } = table;
+  return (
+    <table>
+      <thead>
+        <tr>
+          {columns.map((columnName, i) => (
+            <td key={i}>{columnName}</td>
+          ))}
+        </tr>
+      </thead>
+
+      <tbody>
+        {
+          values.map((row, i) => (
+            <tr key={i}>
+              {row.map((value, i) => (
+                <td key={i}>{value}</td>
+              ))}
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+  );
 }
